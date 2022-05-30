723d90ab6c9991cbfb2265b2c0a3e4a845487b94
hongxin <aragakikin@foxmail.com>
2022-05-30 08:59:05 +0800

    Leetcode 1022

---
+++ src/SumofRootToLeafBinaryNumbers.ts
@@ -0,0 +1,46 @@
+// LeetCode 1022 
+
+// You are given the root of a binary tree where each node has a value 0 or 1. 
+// Each root-to-leaf path represents a binary number starting with the most significant bit.
+
+// For example, if the path is 0 -> 1 -> 1 -> 0 -> 1, then this could represent 01101 in binary, which is 13.
+// For all leaves in the tree, consider the numbers represented by the path from the root to that leaf. Return the sum of these numbers.
+
+// The test cases are generated so that the answer fits in a 32-bits integer.
+
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     val: number
+ *     left: TreeNode | null
+ *     right: TreeNode | null
+ *     constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.left = (left===undefined ? null : left)
+ *         this.right = (right===undefined ? null : right)
+ *     }
+ * }
+ */
+
+class TreeNode {
+    val: number
+    left: TreeNode | null
+    right: TreeNode | null
+    constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+        this.val = (val===undefined ? 0 : val)
+        this.left = (left===undefined ? null : left)
+        this.right = (right===undefined ? null : right)
+    }
+}
+
+function sumRootToLeaf(root: TreeNode | null): number {
+    const dfs = (root : TreeNode, val : number) => {
+        if (!root) return 0;
+        
+        val = (val << 1) | root.val;
+        if (!root.left&& !root.right) return val;
+
+        return dfs(root.left, val) + dfs(root.right, val);
+    }
+    return dfs(root, 0);
+};
